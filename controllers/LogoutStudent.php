04937b922652a374e6c120660fa082845d599927
ryuk <ryuk@gmail.com>
2025-04-06 22:38:49 +0800

    s

--- controllers/LogoutStudent.php
+++ controllers/LogoutStudent.php
@@ -2,6 +2,6 @@
 session_start(); // Start the session
 session_unset(); // Remove all session variables
 session_destroy(); // Destroy the session
-header("Location: ../index.php"); // Redirect to login page
+header("Location: ../"); // Redirect to login page
 exit();
 ?>
